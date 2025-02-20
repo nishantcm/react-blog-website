2d121a5183f7c7af6908ecb879532f8fb1dff47e
cmnishant <cmnishantwork@gmail.com>
2025-02-21 00:39:15 +0530

    Updated the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,39 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Blogs from './pages/Blogs.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Services from './pages/Services.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      },
+      {
+        path: "/services",
+        element: <Services />
+      },
+
+    ]
   },
 ]);
 
