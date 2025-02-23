668ebd21075bc1d6c3cb378705486f658d3a0a23
cmnishant <cmnishantwork@gmail.com>
2025-02-23 05:59:20 +0530

    Created a Navbar.jsx file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    // navItems
+    const navItems = [
+        {path: "/", link: "Home"},
+        {path: "/services", link: "Services"},
+        {path: "/about", link: "About"},
+        {path: "/blogs", link: "Blogs"},
+        {path: "/contact", link: "Contact"},
+    ]
+  return (
+    <header className="bg-black">
+        <nav className="px-4 py-4">
+            <a href="/" className="text-xl font-bold text-white">Design
+                <span className="text-orange-500" >DK</span>
+            </a>
+            {/* navItems for lg devices */}
+            <ul className="md:flex gap-12 text-lg hidden">
+                {
+                    navItems.map(({path, link}) => 
+                        <li className="text-white" key={path}>
+                            <NavLink to={path}>{link}</NavLink>
+                        </li> 
+                    )
+                }
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
