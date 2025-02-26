9f487fa126f5460b05f1f157f2ad22131187fed5
cmnishant <cmnishantwork@gmail.com>
2025-02-26 23:59:35 +0530

    Created the Banner.jsx file

---
+++ src/components/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="px-4 py-32 bg-black mx-auto">
+        <div className="text-white text-center">
+            <h1 className="text-5xl lg:text-7xl leading-snug font-bold mb-5">Welcome to Our Blog</h1>
+            <p className="text-gray-100 lg:w-3/5 mx-auto mb-5">Start your blog today and join a community of writers and readers who are
+                passionate about sharing their stories and ideas. We offer everything you need
+                to get started, from helpful tips and tutorials.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
