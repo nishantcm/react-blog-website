ebc7afa9aa30d89d89e14c989c37096ed2151acc
cmnishant <cmnishantwork@gmail.com>
2025-03-04 06:08:16 +0530

    Updated the BlogCard.jsx file

--- src/components/BlogCards.jsx
+++ src/components/BlogCards.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const BlogCards = () => {
+const BlogCards = ({blogs}) => {
+  const filteredBlogs = blogs;
+  console.log(filteredBlogs)
   return (
-    <div>BlogCards</div>
+    <div>
+      {
+        filteredBlogs.map((blog) => <Link key={blog.id}>
+          <div>
+            <img src={blog.image} alt="" className="w-full" />
+          </div>
+          <h3 class="mt-4 mb-2 font-bold hover:text-blue-600 cursor-pointer">{blog.title}</h3>
+          <p className="mb-2">{blog.author}</p>
+        </Link>)
+      }
+    </div>
   )
 }
 
