bf13c21acbe5914cfff8f2b71e69398a1d478446
cmnishant <cmnishantwork@gmail.com>
2025-03-05 06:59:43 +0530

    Updated the Blogpage.jsx file

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -3,18 +3,38 @@ import BlogCards from './BlogCards';
 
 const BlogPage = () => {
     const [blogs, setBlogs] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 12 // blogs per page
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [activeCategory, setActiveCategory] = useState(null);
 
     useEffect(() => {
         async function fetchBlogs() {
-            let url = `http://localhost:5000/blogs`;
+            let url = `http://localhost:5000/blogs?pages=${currentPage}&limit=${pageSize}`;
 
+            // if filter by category
+            if(selectedCategory) {
+                url +=`&category=${selectedCategory}`;
+            }
             const response = await fetch(url);
             const data = await response.json();
             setBlogs(data);
         }
 
         fetchBlogs();
-    }, [])
+    }, [currentPage, pageSize, selectedCategory])
+
+    // page changing btn
+    const handlePage= (pageNumber) => {
+        setCurrentPage(pageNumber);
+    }
+    
+    const handleCategoryChange= (category) => {
+        setSelectedCategory(category);
+        setCurrentPage(1);
+        setActiveCategory(category);
+    }
+
   return (
     <div>
         {/* category section */}
@@ -22,7 +42,7 @@ const BlogPage = () => {
 
         {/* blogCards section */}
         <div>
-            <BlogCards blogs={blogs}/>
+            <BlogCards blogs={blogs} currentPage={currentPage} selectedCategory={selectedCategory} pageSize={pageSize}/>
         </div>
 
         {/* pagination section */}
