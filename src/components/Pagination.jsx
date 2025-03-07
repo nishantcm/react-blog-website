f0af176051beefdf5f3a94a9a8c0446a1ffd1452
cmnishant <cmnishantwork@gmail.com>
2025-03-07 06:46:56 +0530

    Updated the Pagination.jsx file for the pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,15 +3,23 @@ import React from 'react'
 const Pagination = ({onPageChange, currentPage, blogs, pageSize}) => {
     const totalPages = Math.ceil(blogs.length / pageSize);
     // console.log(totalPages)
-    const renderPaginationLins = () =>{
-        return Array.from ({length: totalPages}, (_, i) => i + 1).map((pageNumber) =>(
+    const renderPaginationLinks = () =>{
+        return Array.from({length: totalPages}, (_, i) => i + 1).map((pageNumber) =>(
             <li className={pageNumber === currentPage ? "activerPagination" : ""} key={pageNumber}>
-                <a href="#" onClick={() => onPageChange(pageNumber) }>{pageNumber}</a>
+                <a href="#" onClick={() => onPageChange(pageNumber)}>{pageNumber}</a>
             </li>
         ))
     }
   return (
-    <div>Pagination</div>
+    <ul className="pagination my-8 flex-wrap gap-4">
+      <li>
+        <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage ===1}>Previous</button>
+      </li>
+      <div className="flex gap-1">{renderPaginationLinks()}</div>
+      <li>
+        <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
+      </li>
+    </ul>
   )
 }
 
