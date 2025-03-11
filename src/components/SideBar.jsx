64d7cf42cd17a6cce879b84d6c76c070b607e3c7
cmnishant <cmnishantwork@gmail.com>
2025-03-11 23:30:37 +0530

    Updated the sidebar file

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -16,7 +16,19 @@ const SideBar = () => {
             <h3 className="text-2xl font-semibold px-4">Latest Blogs</h3>
             <div>
                 {
-                    popularBlogs.slice(0, 5).map(blog => <div key={blog.id}>
+                    popularBlogs.slice(0, 5).map(blog => <div key={blog.id} className="my-5 border-b-2 border-spacing-2 px-4">
+                        <h4 className="font-medium mb-2">{blog.title}</h4>
+                        <Link to="/" className="text-base pb-2 hover:text-orange-500 inline-flex items-center py-1">Read Now <FaArrowRight className="mt-1 ml-2"/></Link>
+                    </div>)
+                }
+            </div>
+        </div>
+                {/* Popular Blog */}
+        <div>
+            <h3 className="text-2xl font-semibold px-4 mt-20">Popular Blogs</h3>
+            <div>
+                {
+                    popularBlogs.slice(6, 10).map(blog => <div key={blog.id} className="my-5 border-b-2 border-spacing-2 px-4">
                         <h4 className="font-medium mb-2">{blog.title}</h4>
                         <Link to="/" className="text-base pb-2 hover:text-orange-500 inline-flex items-center py-1">Read Now <FaArrowRight className="mt-1 ml-2"/></Link>
                     </div>)
