ac6c7ad925c3d585fb2f8fd29c6ea94f2f64ad7a
cmnishant <cmnishantwork@gmail.com>
2025-03-11 23:32:19 +0530

    Updated the Home with connection of Blog page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Banner from '../components/Banner.jsx'
+import BlogPage from '../components/BlogPage.jsx'
 
 const Home = () => {
   return (
     <div>
       <Banner />
+
+      <div className="max-w-7xl mx-auto">
+        <BlogPage />
+      </div>
     </div>
   )
 }
